1c7fd0bce5349c681e71d2838b986b15cbd0f740
Ali Valizadeh <ali.valizadeh.h@gmail.com>
2021-11-03 21:42:26 +0100

    Initial commit

---
+++ Source/PawnInteraction/PawnInteractionGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PawnInteractionGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PAWNINTERACTION_API APawnInteractionGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
